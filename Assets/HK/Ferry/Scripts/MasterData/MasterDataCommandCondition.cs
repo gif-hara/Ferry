cb060e02adac97b699aa0d04630574789bd6a465
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2020-07-08 01:38:30 +0900

    MasterDataCommandCondition追加

---
+++ Assets/HK/Ferry/Scripts/MasterData/MasterDataCommandCondition.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using HK.Ferry.ActorControllers;
+using HK.Ferry.BattleControllers;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Ferry.Database
+{
+    /// <summary>
+    /// コマンド実行条件のデータを持つ<see cref="MasterData{T, R}"/>
+    /// </summary>
+    [CreateAssetMenu(menuName = "Ferry/MasterData/CommandComdition")]
+    public sealed class MasterDataCommandCondition : MasterData<MasterDataCommandCondition, MasterDataCommandCondition.Record>
+    {
+        [Serializable]
+        public sealed class Record : IIdHolder
+        {
+            [SerializeField]
+            private int id = 0;
+
+            [SerializeReference, SubclassSelector]
+            private ICommandCondition condition = default;
+
+            public int Id => this.id;
+
+            public ICommandCondition Condition => this.condition;
+        }
+    }
+}
