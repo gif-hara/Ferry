0953ed159304d86484b4634b99a93e62a9549980
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2020-07-08 01:38:06 +0900

    ICommandCondition追加

---
+++ Assets/HK/Ferry/Scripts/Battle/Commands/Constant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Ferry.BattleControllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Serializable]
+    public sealed class Constant : ICommandCondition
+    {
+        [SerializeField]
+        private bool constant;
+    }
+}
