0c81d8569d67fe72a5c99ac2809fcccd4325f9a2
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2020-07-09 00:57:58 +0900

    CommandBlueprint追加

---
+++ Assets/HK/Ferry/Scripts/Battle/Commands/CommandBlueprint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Ferry.CommandData
+{
+    /// <summary>
+    /// <see cref="CommandNode"/>をまとめるクラス
+    /// </summary>
+    [Serializable]
+    public sealed class CommandBlueprint
+    {
+        [SerializeField]
+        private List<CommandNode> nodes = default;
+        public List<CommandNode> Nodes => this.nodes;
+    }
+}
---
+++ Assets/HK/Ferry/Scripts/MasterData/MasterDataCommandBlueprint.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using HK.Ferry.CommandData;
+using UnityEngine;
+
+namespace HK.Ferry.Database
+{
+    /// <summary>
+    /// <see cref="CommandBlueprint"/>データを持つ<see cref="MasterData{T, R}"/>
+    /// </summary>
+    [CreateAssetMenu(menuName = "Ferry/MasterData/CommandBlueprint")]
+    public sealed class MasterDataCommandBlueprint : MasterData<MasterDataCommandBlueprint, MasterDataCommandBlueprint.Record>
+    {
+        [Serializable]
+        public sealed class Record : IIdHolder
+        {
+            [SerializeField]
+            private int id = 0;
+
+            [SerializeField]
+            private CommandBlueprint blueprint = default;
+
+            public int Id => this.id;
+
+            public CommandBlueprint Blueprint => this.blueprint;
+        }
+    }
+}
