d67a2cda81a1b81f87bc8b36bbc18d11a8372c1a
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2020-12-15 22:46:46 +0900

    AddPower実装

---
+++ Assets/HK/Ferry/Scripts/Commands/AddPower.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Assertions;
+using static HK.Ferry.Constants;
+
+namespace HK.Ferry
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Serializable]
+    public sealed class AddPower : ICommand
+    {
+        [SerializeField]
+        private PowerType powerType = default;
+
+        [SerializeField]
+        private float value = default;
+
+        public IObservable<Unit> Invoke(BattleCharacter attacker, BattleCharacter target)
+        {
+            return Observable.Defer(() =>
+            {
+                attacker.CurrentSpec.Status.AddPower(value, powerType);
+                return Observable.ReturnUnit();
+            });
+        }
+    }
+}
