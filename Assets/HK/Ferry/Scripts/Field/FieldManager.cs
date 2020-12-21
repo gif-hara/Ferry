81e1af1ffe317745e21a60c249e83b6d5e13958a
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2020-12-21 21:30:01 +0900

    とりあえずフィールドを生成するように

---
+++ Assets/HK/Ferry/Scripts/Field/FieldManager.cs
@@ -0,0 +1,41 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+namespace HK.Ferry.FieldSystems
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class FieldManager : MonoBehaviour
+    {
+        [SerializeField]
+        private FieldData debugFieldData = default;
+
+        [SerializeField]
+        private GridLayoutGroup gridLayoutGroup = default;
+
+        [SerializeField]
+        private FieldCellButtonController fieldCellButtonControllerPrefab = default;
+
+        private void Start()
+        {
+            gridLayoutGroup.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+            gridLayoutGroup.constraintCount = debugFieldData.width;
+            for (var y = 0; y < debugFieldData.height; y++)
+            {
+                for (var x = 0; x < debugFieldData.width; x++)
+                {
+                    var controller = Instantiate(fieldCellButtonControllerPrefab, gridLayoutGroup.transform, false);
+                    controller.Button.OnClickAsObservable()
+                        .Subscribe(_ =>
+                        {
+                            Debug.Log(debugFieldData);
+                        })
+                        .AddTo(controller);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/HK/Ferry/Scripts/UI/FieldCellButtonController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+namespace HK.Ferry
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class FieldCellButtonController : MonoBehaviour
+    {
+        [SerializeField]
+        private Button button = default;
+        public Button Button => button;
+    }
+}
